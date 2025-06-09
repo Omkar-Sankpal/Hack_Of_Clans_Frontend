5b620d93ef223bd090cb9fffcb1a9aa674236ab5
Omkar Sankpal <omkarsankpal2004.21@gmail.com>
2025-06-09 12:42:21 +0530

    Changed loading spinner

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -3,7 +3,7 @@ import {motion} from 'framer-motion';
 
 const LoadingSpinner = () => {
   return (
-    <div className='min-h-screen bg-gradient-to-br from-gray-900 to-violet-900 flex items-center justify-center relative overflow-hidden'>
+    <div className='min-h-screen bg-gradient-to-br from-gray-900 to-violet-900 flex flex-col items-center justify-center relative overflow-hidden'>
     <motion.div
         className='w-16 h-16 border-4 border-t-4 border-t-purple-500 border-white rounded-full'
         animate = {{rotate: 360}}
